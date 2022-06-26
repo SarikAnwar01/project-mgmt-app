8cf8b7acd2146691683378042501c449d55d3ef2
Sarik Anwar <sarikanwar007@gmail.com>
2022-06-26 17:25:19 +0545

    Apollo cache implementation and coros in backend

---
+++ client/src/components/ClientRow.jsx
@@ -0,0 +1,30 @@
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutation";
+import { GET_CLIENTS } from "../queries/clientsQueries";
+const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter((c) => c.id !== deleteClient.id) },
+      });
+    },
+  });
+  return (
+    <tr>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+};
+export default ClientRow;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
@@ -12,6 +13,8 @@ const app = express();
 
 // Connect to MongoDB
 connectDB();
+
+app.use(cors());
 
 app.use("/graphql", graphqlHTTP({
     schema: schema,
